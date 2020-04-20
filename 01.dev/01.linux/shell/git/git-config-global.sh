0edc91ecd64b1861bcff22223206df9cf7a09d31
yczcc <yczcc@hotmail.com>
2020-04-20 20:25:02 +0800

    modify git shell

--- 01.dev/01.linux/shell/git/git-config-global.sh
+++ 01.dev/01.linux/shell/git/git-config-global.sh
@@ -2,5 +2,6 @@
 git config --global user.name "yczcc"
 git config --global user.email "yczcc@hotmail.com"
 git config --global credential.helper 'cache --timeout=3600'
+git config --global core.fileMode false
 
 git config --list
